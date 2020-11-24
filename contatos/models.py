5a10812c52b8c9dc5b3403a0ee8f0f3e36d2c6dc
Luciano <lopezz.lucio85@gmail.com>
2020-11-24 16:19:56 -0300

    criado models

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    cel = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100, blank=True)
+    comentario = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
